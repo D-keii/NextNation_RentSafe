660d37d2bc32c7b882f85e795d6d3b4936795377
Daphne Chu <daphnechu28@gmail.com>
2025-12-10 15:26:51 +0800

    Added a mocked redirect url in routes

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,4 +1,32 @@
-from flask import Blueprint, request, jsonify
+from flask import request, url_for, jsonify, redirect, render_template_string
+from . import api_bp
+from db.db import db
+from db.db_tables import Tenant
+# Genrate unique session id
+import uuid
+import json
 
-api_bp = Blueprint("api", __name__)
+# Store mapping session ID to user profiles
+MOCK_TOKENS = {}
+
+# Just to get the redirect url
+@api_bp.route("/auth/login-mydigitalid", methods=["GET"])
+def login_mydigitalid():
+    session_id = str(uuid.uuid4())
+
+    # Mock a session id
+    MOCK_TOKENS[session_id] = {
+        "name": "NextNation",
+        "ic": "000000-00-0000",
+        "email": "next_nation@gmail.com",
+        "verified": True
+    }
+
+    redirect_url = url_for(
+        "api_bp.mock_mydigitalid_page",
+        session=session_id,
+        _external=True
+    )
+    return jsonify({"redirect_url": redirect_url})
+
 
--- backend/db/db_tables.py
+++ backend/db/db_tables.py
@@ -1,4 +1,4 @@
-from db import db
+from db.db import db
 from sqlalchemy import UniqueConstraint
 from sqlalchemy import func
 from datetime import datetime
